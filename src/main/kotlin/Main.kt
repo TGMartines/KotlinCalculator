800f0683dcb6b7ef20c6b9b73da20be77ec33af4
Thiago Martines <142542192+TGMartines@users.noreply.github.com>
2023-08-30 13:42:50 -0400

    Calculadora em Kotlin Melhorada

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,7 @@
-fun soma(num1: Int, num2: Int): Int {
-    return num1 + num2
-}
-fun subtracao(num1: Int, num2: Int): Int {
-    return num1 - num2
-}
-fun multiplicacao(num1: Int, num2: Int): Int {
-    return num1 * num2
-}
-fun divisao(num1: Int, num2: Int): Int {
-    return (num1 / num2)
-}
+fun soma(num1: Int, num2: Int) = num1 + num2
+fun subtracao(num1: Int, num2: Int) = num1 - num2
+fun multiplicacao(num1: Int, num2: Int) = num1 * num2
+fun divisao(num1: Int, num2: Int) = (num1 / num2)
 
 
 fun main() {
